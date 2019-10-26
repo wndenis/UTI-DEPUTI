cd4ec0169b3a89afc725370414f8c4db9a268307
Nick <nikitamm59rus@gmail.com>
2019-10-26 15:59:47 +0300

    Some css manipulations

--- Animation.js
+++ Animation.js
@@ -1,15 +1,15 @@
 var getUrlParameter = function getUrlParameter(sParam) {
   var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-      sURLVariables = sPageURL.split('&'),
-      sParameterName,
-      i;
+    sURLVariables = sPageURL.split("&"),
+    sParameterName,
+    i;
 
   for (i = 0; i < sURLVariables.length; i++) {
-      sParameterName = sURLVariables[i].split('=');
+    sParameterName = sURLVariables[i].split("=");
 
-      if (sParameterName[0] === sParam) {
-          return sParameterName[1] === undefined ? true : sParameterName[1];
-      }
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
+    }
   }
 };
 
@@ -22,12 +22,11 @@ var numStars = 0;
 var acceleration = 1;
 var starsToDraw = (field.width * field.height) / 200;
 if (getUrlParameter("stars")) {
-starsToDraw = getUrlParameter("stars");
+  starsToDraw = getUrlParameter("stars");
 }
 if (getUrlParameter("accel")) {
-acceleration = getUrlParameter("accel");
-} 
-
+  acceleration = getUrlParameter("accel");
+}
 
 function Star() {
   this.X = field.width / 2;
@@ -38,13 +37,11 @@ function Star() {
 
   var start = 0;
 
-  if (field.width > field.height)
-      start = field.width;
-  else
-      start = field.height;
+  if (field.width > field.height) start = field.width;
+  else start = field.height;
 
-  this.X += this.SX * start / 10;
-  this.Y += this.SY * start / 10;
+  this.X += (this.SX * start) / 10;
+  this.Y += (this.SY * start) / 10;
 
   this.W = 1;
   this.H = 1;
@@ -59,51 +56,56 @@ function Star() {
   this.C = "#ffffff";
 }
 
-Star.prototype.Draw = function () {
+Star.prototype.Draw = function() {
   this.X += this.SX;
-  this.Y += this.SY
-  
+  this.Y += this.SY;
+
   this.SX += this.SX / (50 / acceleration);
   this.SY += this.SY / (50 / acceleration);
 
   this.age++;
 
-  if (this.age == Math.floor(50 / acceleration) | this.age == Math.floor(150 / acceleration) | this.age == Math.floor(300 / acceleration)) {
-      this.W++;
-      this.H++;
+  if (
+    (this.age == Math.floor(50 / acceleration)) |
+    (this.age == Math.floor(150 / acceleration)) |
+    (this.age == Math.floor(300 / acceleration))
+  ) {
+    this.W++;
+    this.H++;
   }
 
-  if (this.X + this.W < 0 | this.X > field.width |
-      this.Y + this.H < 0 | this.Y > field.height)
-    {
-      delete stars[this.ID];
-      numStars--;
-    }
+  if (
+    (this.X + this.W < 0) |
+    (this.X > field.width) |
+    (this.Y + this.H < 0) |
+    (this.Y > field.height)
+  ) {
+    delete stars[this.ID];
+    numStars--;
+  }
 
   f.fillStyle = this.C;
   f.fillRect(this.X, this.Y, this.W, this.H);
-}
+};
 
 field.width = window.innerWidth;
 field.height = window.innerHeight;
 
 function draw() {
-  if (field.width != window.innerWidth)
-      field.width = window.innerWidth;
-  if (field.height != window.innerHeight)
-      field.height = window.innerHeight;
+  if (field.width != window.innerWidth) field.width = window.innerWidth;
+  if (field.height != window.innerHeight) field.height = window.innerHeight;
 
   // Play with the "a" value to create streams...it's fun!
   f.fillStyle = "rgba(0, 0, 0, 0.8)";
   f.fillRect(0, 0, field.width, field.height);
 
   for (var i = numStars; i < starsToDraw; i++) {
-      new Star();
-      numStars++;
+    new Star();
+    numStars++;
   }
 
   for (var star in stars) {
-      stars[star].Draw();
+    stars[star].Draw();
   }
 }
 
